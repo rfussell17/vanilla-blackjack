16ae8db3b6e02a96859acd1cbea3b5a033f078ed
Robin F <rfussell17@gmail.com>
2021-10-09 14:48:30 -0700

    add basic game logic

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-let firstCard = 4;
+let firstCard = 13;
 let secondCard = 8;
-
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+
+if (sum < 21) {
+  console.log("Do you want to draw a new card?");
+} else if (sum === 21) {
+  console.log("Blackjack!");
+  hasBlackJack = true;
+} else {
+  console.log("You Lose!");
+  isAlive = false
+}
+
+console.log(hasBlackJack)
