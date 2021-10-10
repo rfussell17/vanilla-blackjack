507ce1be114695dd54e7d15a33191f70462e3438
Robin F <rfussell17@gmail.com>
2021-10-10 12:40:12 -0700

    display firstCard, secondCard

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById('message-el');
-let sumEl = document.getElementById("sum-el")
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame(){
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    sumEl.textContent = "Sum: " + sum;
     if ( sum < 21) {
         message = "Do you want to draw a new card?";
       } else if (sum === 21) {
@@ -18,5 +21,4 @@ function startGame(){
         isAlive = false
       }
       messageEl.textContent = message;
-      sumEl.textContent = "Sum: " + sum;
 }
