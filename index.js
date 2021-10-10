06e3c0570d753e4a819167e4898224a57e396cf5
Robin F <rfussell17@gmail.com>
2021-10-10 14:31:12 -0700

    converting to OOP structure

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-let card = [];
-let sum = 0;
+class Person {
+    constructor(firstCard, secondCard){
+        this.firstCard = firstCard,
+        this.secondCard = secondCard
+    }
+}
+
+let player = new Person(getRandomCard(), getRandomCard());
+player.cards = [player.firstCard, player.secondCard]
+player.name = "Robin"
+player.chips = 333;
+
+let house = new Person(getRandomCard(), getRandomCard());
+house.cards = [house.firstCard, house.secondCard]
+
+// let cards = [];
+let houseSum = 0;
+let playerSum
 let hasBlackJack = false;
 let isAlive = false;
 let message = "";
-let messageEl = document.getElementById('message-el');
-let sumEl = document.getElementById("sum-el");
-let cardsEl = document.getElementById("cards-el");
 
-let player = {
-    name: "Robin",
-    chips: 125
-}
+let messageEl = document.getElementById('message-el');
 
 let playerEl = document.getElementById("player-el")
+let playerSumEl = document.getElementById("player-sum-el");
+let playerCardsEl = document.getElementById("player-cards-el");
+
+let houseEl = document.getElementById("house-el")
+let HouseSumEl = document.getElementById("house-sum-el");
+let HouseCardsEl = document.getElementById("house-cards-el");
+
 playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard(){
@@ -28,16 +45,26 @@ function startGame(){
     isAlive = true;
     let firstCard = getRandomCard();
     let secondCard = getRandomCard();
-    cards = [firstCard, secondCard];
+    player.cards = [firstCard, secondCard];
+    house.cards = [firstCard, secondCard];
     sum = firstCard + secondCard;
     renderGame();
 }
 
+function renderHouse(){
+
+}
+
+function renderPlayer(){
+
+}
+
+
 function renderGame(){
-    let displayCards = cards.map(c => cardsEl.textContent = " " + c);
-    cardsEl.textContent = "Cards: " + displayCards;
-    sumEl.textContent = "Sum: " + sum;
-    if ( sum < 21) {
+    let displayHouseCards = house.cards.map(c => cardsEl.textContent = " " + c);
+    houseCardsEl.textContent = "Cards: " + displayHouseCards;
+    houseSumEl.textContent = "Sum: " + houseSum;
+    if ( houseSum < 21) {
         message = "Do you want to draw a new card?";
       } else if (sum === 21) {
         message = "Blackjack!"; 
@@ -53,7 +80,8 @@ function newCard(){
     if(isAlive && hasBlackJack === false){
         let card = getRandomCard();
         sum += card;
-        cards.push(card);
+        player.cards.push(card);
+        house.cards.push(card);
         renderGame();
     } 
 }
